79d1916ab9f8e484303dcf0e4041d0d9e5dd15c0
leandro <leandrosousajesus@gmail.com>
2024-11-26 01:07:31 -0300

    Versão Br+TI

--- aplicativo/prova.py
+++ aplicativo/prova.py
@@ -8,14 +8,16 @@ import sys
 
 # Inicializa o mixer de áudio
 pygame.mixer.init()
-# Carrega o arquivo de som (Alarme)
-pygame.mixer.music.load("alarmenavio.mp3")
+
+# Carrega os arquivos de som
+pygame.mixer.music.load("alarmenavio.mp3")  # Alarme da boca
+som_olho = pygame.mixer.Sound("businadj.mp3")  # Alarme dos olhos
+som_assobio = pygame.mixer.Sound("assobio.mp3")  # Som para rosto detectado
 
 # Pontos dos olhos e boca
 p_olho_esq = [385, 380, 387, 373, 362, 263]
 p_olho_dir = [160, 144, 158, 153, 33, 133]
 p_olhos = p_olho_esq + p_olho_dir
-
 p_boca = [82, 87, 13, 14, 312, 317, 78, 308]
 
 # Função EAR (Eye Aspect Ratio)
@@ -26,9 +28,7 @@ def calculo_ear(face, p_olho_dir, p_olho_esq):
         face_dir = face[p_olho_dir, :]
 
         ear_esq = (np.linalg.norm(face_esq[0] - face_esq[1]) + np.linalg.norm(face_esq[2] - face_esq[3])) / (2 * (np.linalg.norm(face_esq[4] - face_esq[5])))
-
         ear_dir = (np.linalg.norm(face_dir[0] - face_dir[1]) + np.linalg.norm(face_dir[2] - face_dir[3])) / (2 * (np.linalg.norm(face_dir[4] - face_dir[5])))
-
     except:
         ear_esq = 0.0
         ear_dir = 0.0
@@ -48,11 +48,12 @@ def calculo_mar(face, p_boca):
 
 # Limiares
 ear_limiar = 0.27
-mar_limiar = 0.1
-dormindo = 0  # Flag para controle dos olhos fechados
+mar_limiar = 0.5
+dormindo = False  # Flag para controle dos olhos fechados
 aberto_boca = False  # Flag para controle da boca aberta
 tempo_olhos_fechados = 0.0  # Tempo que os olhos ficaram fechados
 tempo_boca_aberta = 0.0  # Tempo que a boca ficou aberta
+contagem_piscadas = 0  # Contagem de piscadas
 
 # Inicializa a câmera
 cap = cv2.VideoCapture(0)
@@ -61,7 +62,16 @@ mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 
 # Estado do som
-som_tocando = False
+som_boca_tocando = False
+som_olho_tocando = False
+som_assobio_tocando = False
+
+# Variáveis para controle de exibição das mensagens
+mensagem_boca = ""
+mensagem_olhos = ""
+tempo_inicio_mensagem_boca = None
+tempo_inicio_mensagem_olhos = None
+duracao_mensagem = 3  # Duração das mensagens em segundos
 
 # Define o tamanho da janela
 window_width = 800
@@ -69,6 +79,10 @@ window_height = 600
 
 cv2.namedWindow('Camera', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('Camera', window_width, window_height)
+
+# Variáveis de tempo para o aviso de rosto
+rosto_detectado = False
+tempo_inicial_aviso = None
 
 with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5) as facemesh:
     while cap.isOpened():
@@ -84,101 +98,136 @@ with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
         if saida_facemesh.multi_face_landmarks:
-            # Detectando rosto
-            print("Rosto detectado")
-            if not som_tocando:
-                pygame.mixer.music.play(-1)  # Toca continuamente
-                som_tocando = True  # Atualiza o estado para som tocando
-        else:
-            print("Nenhum rosto detectado")
-            if som_tocando:
-                pygame.mixer.music.stop()  # Para o som
-                som_tocando = False  # Atualiza o estado para som parado
+            rosto_detectado = True
+            tempo_inicial_aviso = time.time()   
 
-        try:
             for face_landmarks in saida_facemesh.multi_face_landmarks:
                 mp_drawing.draw_landmarks(
                     frame,
                     face_landmarks,
                     mp_face_mesh.FACEMESH_CONTOURS,
-                    landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255, 102, 102), thickness=1, circle_radius=1),
-                    connection_drawing_spec=mp_drawing.DrawingSpec(color=(102, 204, 0), thickness=1, circle_radius=1)
+                    landmark_drawing_spec=mp_drawing.DrawingSpec(color=(0, 0, 220), thickness=1, circle_radius=1),
+                    connection_drawing_spec=mp_drawing.DrawingSpec(color=(0, 204, 0), thickness=1, circle_radius=1)
                 )
                 
                 face = face_landmarks.landmark
                 
-                for id_coord, coord_xyz in enumerate(face):
-                    if id_coord in p_olhos:
-                        coord_cv = mp_drawing._normalized_to_pixel_coordinates(coord_xyz.x, coord_xyz.y, largura, comprimento)
-                        cv2.circle(frame, coord_cv, 2, (255, 0, 0), -1)
-                    if id_coord in p_boca:
-                        coord_cv = mp_drawing._normalized_to_pixel_coordinates(coord_xyz.x, coord_xyz.y, largura, comprimento)
-                        cv2.circle(frame, coord_cv, 2, (255, 0, 0), -1)
-
-                # Chamada do EAR e print
                 ear = calculo_ear(face, p_olho_dir, p_olho_esq)
-                cv2.rectangle(frame, (0, 1), (290, 140), (58, 58, 55), -1)
-                cv2.putText(frame, f"EAR: {round(ear, 2)}", (1, 24),
-                            cv2.FONT_HERSHEY_DUPLEX,
-                            0.9, (255, 255, 255), 2)
-
-                # Chamada do MAR e print
                 mar = calculo_mar(face, p_boca)
-                cv2.putText(frame, f"MAR: {round(mar, 2)} { 'abertos' if mar >= mar_limiar else  'fechados '}", (1, 50),
-                            cv2.FONT_HERSHEY_DUPLEX,
-                            0.9, (255, 255, 255), 2)
-
-                # Verificação da condição dos olhos fechados
-                if ear < ear_limiar:
-                    if dormindo == 0:
-                        t_inicial = time.time()  # Marca o tempo inicial
-                    dormindo = 1  # Olhos estão fechados
-                if dormindo == 1 and ear >= ear_limiar:
-                    dormindo = 0  # Olhos abriram
-                t_final = time.time()
-
-                tempo = (t_final - t_inicial) if dormindo == 1 else 0.0
-                tempo_olhos_fechados = tempo
-                cv2.putText(frame, f"Tempo olhos fechados: {round(tempo_olhos_fechados, 3)}", (1, 80),
-                            cv2.FONT_HERSHEY_DUPLEX,
-                            0.9, (255, 255, 255), 2)
-                
-                # Exibe alerta se os olhos ficarem fechados por mais de 1.5 segundos
-                if tempo_olhos_fechados >= 1.5:
-                    cv2.rectangle(frame, (30, 400), (610, 452), (109, 233, 219), -1)
-                    cv2.putText(frame, f"Olhos fechados por muito tempo!", (80, 435),
-                                cv2.FONT_HERSHEY_DUPLEX,
-                                0.85, (58, 58, 55), 1)
-                    if not som_tocando:
-                        pygame.mixer.music.play(-1)  # Toca o som de alerta
-                        som_tocando = True
+                cv2.rectangle(frame,(0,1),(290,140),(58,58,55),-1)
 
                 # Verificação da condição da boca aberta
                 if mar > mar_limiar:
                     if not aberto_boca:
-                        tempo_boca_aberta = time.time()  # Marca o tempo inicial da boca aberta
+                        tempo_inicial_boca_aberta = time.time()
                         aberto_boca = True
+                    else:
+                        tempo_boca_aberta = time.time() - tempo_inicial_boca_aberta
+                    estado_boca = "aberta"
                 else:
-                    if aberto_boca:
-                        tempo_boca_aberta = 0.0  # Resetar tempo da boca fechada
-                        aberto_boca = False
+                    tempo_boca_aberta = 0.0
+                    aberto_boca = False
+                    estado_boca = "fechada"
 
-                # Exibe o tempo da boca aberta
-                cv2.putText(frame, f"Tempo com a boca aberta: {round(tempo_boca_aberta, 3)}", (1, 110),
-                            cv2.FONT_HERSHEY_DUPLEX,
-                            0.9, (55, 255, 255), 2)
-                
-        except Exception as e:
-            print("Erro:", e)
+                # Verificação da condição dos olhos fechados
+                if ear < ear_limiar:
+                    if not dormindo:
+                        tempo_inicial_olhos_fechados = time.time()
+                        dormindo = True
+                    else:
+                        tempo_olhos_fechados = time.time() - tempo_inicial_olhos_fechados
+                    estado_olho = "fechados"
+                else:
+                    if dormindo:
+                        contagem_piscadas += 1
+                    tempo_olhos_fechados = 0.0
+                    dormindo = False
+                    estado_olho = "abertos"
 
-        # Exibindo o frame com as marcações
+                # Exibir estados e tempos na tela
+                cv2.rectangle(frame, (0, 0), (290, 100), (58, 58, 55), -1)  # Fundo do quadro
+                cv2.putText(frame, f"MAR - Boca: {estado_boca} - {round(tempo_boca_aberta, 2)}s", 
+                            (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
+                cv2.putText(frame, f"EAR - Olhos: {estado_olho} - {round(tempo_olhos_fechados, 2)}s", 
+                            (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
+                cv2.putText(frame, f"Piscadas: {contagem_piscadas}", 
+                            (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
+
+
+                # Alarme para boca aberta
+                if tempo_boca_aberta >= 1.5 and not som_boca_tocando:
+                    mensagem_boca = f"Alerta de sono, bocejando!"
+                    tempo_inicio_mensagem_boca = time.time()
+                    pygame.mixer.music.play(-1)
+                    som_boca_tocando = True
+                elif tempo_boca_aberta == 0.0 and som_boca_tocando:
+                    pygame.mixer.music.stop()
+                    som_boca_tocando = False
+
+                # Alarme para olhos fechados
+                if tempo_olhos_fechados >= 1.0 and not som_olho_tocando:
+                    mensagem_olhos = "Olhos fechados por muito tempo!"
+                    tempo_inicio_mensagem_olhos = time.time()
+                    som_olho.play()
+                    som_olho_tocando = True
+                elif tempo_olhos_fechados == 0.0 and som_olho_tocando:
+                    som_olho.stop()
+                    som_olho_tocando = False
+
+                # Exibir mensagens no canto inferior da tela
+                altura, largura, _ = frame.shape
+                if mensagem_boca and (time.time() - tempo_inicio_mensagem_boca <= duracao_mensagem):
+                    cv2.rectangle(frame, (0, altura - 70), (largura, altura), (0, 0, 0), -1)  # Fundo preto
+                    cv2.putText(frame, mensagem_boca, (10, altura - 30), cv2.FONT_HERSHEY_DUPLEX, 0.9, (255, 255, 255), 2)
+                else:
+                    mensagem_boca = ""  # Limpa a mensagem após a duração
+
+                if mensagem_olhos and (time.time() - tempo_inicio_mensagem_olhos <= duracao_mensagem):
+                    cv2.rectangle(frame, (0, altura - 140), (largura, altura - 70), (0, 0, 0), -1)  # Fundo preto
+                    cv2.putText(frame, mensagem_olhos, (10, altura - 100), cv2.FONT_HERSHEY_DUPLEX, 0.9, (255, 255, 255), 2)
+                else:
+                    mensagem_olhos = ""  # Limpa a mensagem após a duração
+
+        else:
+            # Atualiza o estado e tempo quando nenhum rosto é detectado
+            if rosto_detectado:
+                tempo_inicial_aviso = time.time()
+            rosto_detectado = False
+
+        # Exibe aviso de rosto detectado ou não detectado no canto superior direito por 5 segundos
+        if tempo_inicial_aviso and (time.time() - tempo_inicial_aviso <= 5):
+            mensagem = "Rosto detectado" if rosto_detectado else "Nenhum rosto detectado"
+            cor_texto = (0, 255, 0) if rosto_detectado else (0, 0, 255)  # Verde para detectado, vermelho para não detectado
+            largura_frame = frame.shape[1]
+            tamanho_texto = cv2.getTextSize(mensagem, cv2.FONT_HERSHEY_DUPLEX, 1.0, 2)[0]  # Obtém o tamanho do texto
+            posicao_x = largura_frame - tamanho_texto[0] - 10  # Calcula a posição x do texto para o canto direito
+            posicao_y = 30  # Altura fixa no canto superior
+            cv2.putText(frame, mensagem, (posicao_x, posicao_y), cv2.FONT_HERSHEY_DUPLEX, 1.0, cor_texto, 2)
+
+
+         # Exibe aviso de rosto detectado ou não detectado no canto superior direito por 5 segundos
+        if tempo_inicial_aviso and (time.time() - tempo_inicial_aviso <= 5):
+            mensagem = "Rosto detectado" if rosto_detectado else "Nenhum rosto detectado"
+            cor_texto = (0, 255, 0) if rosto_detectado else (0, 0, 255)  # Verde para detectado, vermelho para não detectado
+            largura_frame = frame.shape[1]
+            tamanho_texto = cv2.getTextSize(mensagem, cv2.FONT_HERSHEY_DUPLEX, 1.0, 2)[0]  # Obtém o tamanho do texto
+            posicao_x = largura_frame - tamanho_texto[0] - 10  # Calcula a posição x do texto para o canto direito
+            posicao_y = 30  # Altura fixa no canto superior
+            cv2.putText(frame, mensagem, (posicao_x, posicao_y), cv2.FONT_HERSHEY_DUPLEX, 1.0, cor_texto, 2)
+
+            # Tocar som de assobio quando rosto detectado
+            if rosto_detectado and not som_assobio_tocando:
+                som_assobio.play()
+                som_assobio_tocando = True
+        else:
+            # Para o som quando o tempo de exibição da mensagem acaba
+            if som_assobio_tocando:
+                som_assobio.stop()
+                som_assobio_tocando = False
+
         cv2.imshow('Camera', frame)
-
-        # Se pressionar 'c', fecha a janela
-        if cv2.waitKey(10) & 0xFF == ord('c'):
+        if cv2.waitKey(5) & 0xFF == 27:
             break
 
-# Finaliza a captura da câmera e fecha a janela
 cap.release()
 cv2.destroyAllWindows()
-
